685b8a154a11caa7eaaa0072de1c7f9cdd9b0ba7
unknown <ade@badr-interactive.com>
2021-06-14 21:46:34 +0700

    commit-pertama

---
+++ src/pages/Login/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../Login/Login.css';
+
+const Login = () => {
+    return (
+    
+        <>
+        <h4>Login</h4>
+        <div className="card">
+            <p>Skybook</p>
+            <h5> Skybook membantu Anda terhubung dan berbagi dengan orang-orang dalam kehidupan Anda.</h5>
+            <hr></hr>
+            <h3>Buat akun baru</h3>
+            <p1>email</p1>
+            <p2>Password</p2>
+            <p3>Login</p3>
+        </div>
+        </>
+    )
+}
+
+export default Login
---
+++ src/pages/Register/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        
+        <div>
+            <p>Register Page</p>
+        </div>
+    )
+}
+
+export default Register
