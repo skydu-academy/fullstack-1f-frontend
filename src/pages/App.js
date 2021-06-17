2419099e92c2cbb5e521d4805c897bad9a013894
unknown <ade@badr-interactive.com>
2021-06-17 20:48:20 +0700

    commit kedua

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react'
-// import GoogleLogin from 'react-google-login'
 import { Routes } from '../config';
 import '../pages/App.css';
 
 
 export class App extends Component {
-  // ResponseGoegle=(response)=> {
-  //   console.log(response);
-  //   console.log(response.profileObj);
-  // }
+  
   render() {
     return (
       <>
         <Routes />
         <div>
-        {/* <GoogleLogin 
-        clientId="189901676536-gsefjrlhkhug3c1l5cgbg3ojdga6cjki.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={this.ResponseGoegle}
-        onFailure={this.ResponseGoegle}
-        cookiePolicy={'single_host_origin'}/> */}
+        
       </div>
     </>
     )
