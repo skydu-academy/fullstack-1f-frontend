5fa0d70487ee24c51c8fd872c05208aff4ab6201
unknown <ade@badr-interactive.com>
2021-06-17 20:43:09 +0700

    commit lanjutan

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
-import {Routes} from '../config';
-function App() {
-  return (
-    <Routes />
-  );
+import React, { Component } from 'react'
+// import GoogleLogin from 'react-google-login'
+import { Routes } from '../config';
+import '../pages/App.css';
+
+
+export class App extends Component {
+  // ResponseGoegle=(response)=> {
+  //   console.log(response);
+  //   console.log(response.profileObj);
+  // }
+  render() {
+    return (
+      <>
+        <Routes />
+        <div>
+        {/* <GoogleLogin 
+        clientId="189901676536-gsefjrlhkhug3c1l5cgbg3ojdga6cjki.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.ResponseGoegle}
+        onFailure={this.ResponseGoegle}
+        cookiePolicy={'single_host_origin'}/> */}
+      </div>
+    </>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import '../Login/Login.css';
+import GoogleLogin from 'react-google-login';
+
 
 const Login = () => {
-    return (
+     let ResponseGoegle=(response)=> {
+        console.log(response);
+        console.log(response.profileObj);
+      }
     
-        <>
+    return (
+      <>
         <h4>Login</h4>
         <div className="card">
             <p>Skybook</p>
@@ -14,9 +20,18 @@ const Login = () => {
             <p1>email</p1>
             <p2>Password</p2>
             <p3>Login</p3>
+            </div>
+            <div className="click">
+            <GoogleLogin 
+        clientId="189901676536-gsefjrlhkhug3c1l5cgbg3ojdga6cjki.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={ResponseGoegle}
+        onFailure={ResponseGoegle}
+        cookiePolicy={'single_host_origin'}/>
         </div>
-        </>
+      </>
     )
-}
+  }
+
 
 export default Login
